c2f1f90e38ffe0a42e11b742c7c3d73fc0126169
Clockwork-Blue <93886483+Clockwork-Blue@users.noreply.github.com>
2023-02-07 14:17:58 +0100

    added internal link card 'Cards'

--- globalJs/global.js
+++ globalJs/global.js
@@ -1,4 +1,4 @@
-function cardMaker(repo, target) {
+function cardMakerI(repo, target) {
     //get main cont div
     let main = document.getElementById("main");
     //make main card
@@ -6,6 +6,32 @@ function cardMaker(repo, target) {
     card.classList.add("card");
     main.appendChild(card);
     card.href= "https://github.com/Clockwork-Blue/" + target;
+    //anchor
+    //img row
+    let cImgDiv = document.createElement("div");
+    cImgDiv.classList.add("img-row");
+    card.appendChild(cImgDiv);
+    let cImg = document.createElement("img");
+    cImg.classList.add("cardImg");
+    let source = "globalImg/" + repo + ".png";
+    cImg.src = source;
+    cImgDiv.appendChild(cImg);
+    //text-row
+    let textRow = document.createElement("div");
+    textRow.classList.add("text-row");
+    textRow.innerText= repo;
+    card.appendChild(textRow);
+    
+}
+https://clockwork-blue.github.io/cards-test/index.html
+function cardMakerE(repo, target) {
+    //get main cont div
+    let main = document.getElementById("main");
+    //make main card
+    let card = document.createElement("a");
+    card.classList.add("card");
+    main.appendChild(card);
+    card.href= target;
     //anchor
     //img row
     let cImgDiv = document.createElement("div");
